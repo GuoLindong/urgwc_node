c9ad3b7f51adc0304e94f4ca1632bcd6981a709b
guolindong <guolindong@gmail.com>
2018-03-27 16:09:08 +0800

    first commit

---
+++ src/pointcloud_transform_rotate.h
@@ -0,0 +1,26 @@
+//
+// Created by guolindong on 18-3-14.
+//
+
+#ifndef PROJECT_POINTCLOUD_TRANSFORM_ROTATE_H
+#define PROJECT_POINTCLOUD_TRANSFORM_ROTATE_H
+
+#include <pcl-1.7/pcl/io/pcd_io.h>
+#include <pcl-1.7/pcl/io/ply_io.h>
+#include <pcl-1.7/pcl/point_cloud.h>
+#include <pcl-1.7/pcl/console/parse.h>
+#include <pcl-1.7/pcl/common/transforms.h>
+
+using namespace std;
+
+//template <typename pcl::PointCloud<T>>
+//void transfor_rotate(double x, double y, double z,
+//                    double roll, double pitch, double yaw,
+//                    T& pointcloud)
+//{
+//
+//}
+
+
+
+#endif //PROJECT_POINTCLOUD_TRANSFORM_ROTATE_H
